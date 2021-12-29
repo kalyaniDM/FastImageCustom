5df007123652c66d8631c9a0d153e6822bd62c59
NehaRuparelgit <82521889+NehaRuparelgit@users.noreply.github.com>
2021-12-29 15:59:36 +0530

    Update and rename FastImageViewModule.java to FastImagePreloaderModule.java

--- android/src/main/java/com/dylanvann/fastimage/FastImagePreloaderModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImagePreloaderModule.java
@@ -12,11 +12,12 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.views.imagehelper.ImageSource;
 
-class FastImageViewModule extends ReactContextBaseJavaModule {
+class FastImagePreloaderModule extends ReactContextBaseJavaModule {
 
-    private static final String REACT_CLASS = "FastImageView";
+    private static final String REACT_CLASS = "FastImagePreloaderManager";
+    private int preloaders = 0;
 
-    FastImageViewModule(ReactApplicationContext reactContext) {
+    FastImagePreloaderModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
 
@@ -24,20 +25,26 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
     public String getName() {
         return REACT_CLASS;
     }
-
     @ReactMethod
-    public void preload(final ReadableArray sources) {
+    public void createPreloader(Promise promise) {
+        promise.resolve(preloaders++);
+    }
+    
+    @ReactMethod
+    public void preload(final int preloaderId, final ReadableArray sources) {
         final Activity activity = getCurrentActivity();
         if (activity == null) return;
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
+                FastImagePreloaderListener preloader = new FastImagePreloaderListener(getReactApplicationContext(), preloaderId, sources.size());
                 for (int i = 0; i < sources.size(); i++) {
                     final ReadableMap source = sources.getMap(i);
                     final FastImageSource imageSource = FastImageViewConverter.getImageSource(activity, source);
 
                     Glide
                             .with(activity.getApplicationContext())
+                            .downloadOnly()
                             // This will make this work for remote and local images. e.g.
                             //    - file:///
                             //    - content://
@@ -48,6 +55,7 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
                                     imageSource.isBase64Resource() ? imageSource.getSource() :
                                     imageSource.isResource() ? imageSource.getUri() : imageSource.getGlideUrl()
                             )
+                            .listener(preloader)
                             .apply(FastImageViewConverter.getOptions(activity, imageSource, source))
                             .preload();
                 }
